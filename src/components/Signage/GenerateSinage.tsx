4a9d571c35774c78a96d8dd38efa73eae9edd1ec
kaita2725 <adriel.chandra@student.umn.ac.id>
2025-12-15 10:34:42 +0700

    add timing

--- src/components/Signage/GenerateSinage.tsx
+++ src/components/Signage/GenerateSinage.tsx
@@ -1,17 +1,21 @@
 import { useMemo } from "react";
 import "./style.css";
+import { useSignage } from "../../features/signage/context/signageCtx";
 type Props = {
   message: string;
 };
 
 const GenerateSignage = ({ message }: Props) => {
+  const { timing } = useSignage();
   const msgMemo = useMemo(() => {
     return message;
   }, [message]);
 
+  const timingMemo = useMemo(() => timing, [timing]);
+
   return (
     <div className="marquee">
-      <span style={{ animation: `marquee ${10}s linear infinite` }}>
+      <span style={{ animation: `marquee ${timingMemo}s linear infinite` }}>
         {msgMemo}
       </span>
     </div>
